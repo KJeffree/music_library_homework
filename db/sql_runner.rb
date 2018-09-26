ec3c1aae6ae0b7f7547c35bf7b4db545ee28cecf
Katie Jeffree <k.jeffree@gmail.com>
2018-09-26 15:34:54 +0100

    first commit - classes, db and sql runner set up

---
+++ db/sql_runner.rb
@@ -0,0 +1,13 @@
+require('pg')
+
+class SqlRunner
+
+  def self.run(sql, values = [])
+    db = PG.connect({dbname: 'music_library', host: 'localhost'})
+    db.prepare('query', sql)
+    results = db.exec_prepared('query', values)
+    db.close
+    return results
+  end
+
+end
---
+++ models/artist.rb
@@ -0,0 +1,10 @@
+class Artist
+
+  def initialize(options)
+    @id = options['id'].to_i
+    @name = options['name']
+  end
+
+
+
+end
